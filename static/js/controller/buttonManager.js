6c5e698035ee76f2c94af5a3f24a1e121826895b
DinuOctavian1 <dinuoctavianstefan@gmail.com>
2022-05-10 19:19:57 +0300

    add new board btn

---
+++ static/js/controller/buttonManager.js
@@ -0,0 +1,19 @@
+import {htmlFactory, htmlTemplates} from "../view/htmlFactory.js";
+import {domManager} from "../view/domManager.js";
+
+export let buttonManager = {
+    loadNewBoardBtn: function () {
+            const btnBuilder = htmlFactory(htmlTemplates.newBoardBtn);
+            const content = btnBuilder();
+            domManager.addChild(".title-container",content);
+            domManager.addEventListener(
+                "#new-board-button",
+                "click",
+                printNew
+            );
+    },
+};
+
+function printNew() {
+    console.log('apasa')
+}
