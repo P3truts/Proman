4949b5f4a7ef4de21408f6b7030c416dbb15a3c8
alexsincai <alex.sincai@yahoo.co.uk>
2022-05-13 11:13:45 +0300

    formatted queries

--- queries.py
+++ queries.py
@@ -3,74 +3,37 @@ import data_manager
 
 def get_board_by_id(board_id):
     return data_manager.execute_select(
-        """
-        select * from boards b
-        where b.id = %(board_id)s
-        ;
-        """,
+        "SELECT * FROM boards b WHERE b.id = %(board_id)s;",
         {"board_id": board_id},
         fetchall=False,
     )
 
 
 def get_card_status(status_id):
-    """
-    Find the first status matching the given id
-    :param status_id:
-    :return: str
-    """
-    status = data_manager.execute_select(
-        """
-        SELECT * FROM statuses s
-        WHERE s.id = %(status_id)s
-        ;
-        """,
+    return data_manager.execute_select(
+        "SELECT * FROM statuses s WHERE s.id = %(status_id)s;",
         {"status_id": status_id},
     )
 
-    return status
-
 
 def get_boards():
-    return data_manager.execute_select(
-        """
-        SELECT * FROM boards
-        ;
-        """
-    )
+    return data_manager.execute_select("SELECT * FROM boards;")
 
 
 def get_cards_for_board(board_id):
-    matching_cards = data_manager.execute_select(
-        """
-        SELECT * FROM cards
-        WHERE cards.board_id = %(board_id)s
-        ;
-        """,
+    return data_manager.execute_select(
+        "SELECT * FROM cards WHERE cards.board_id = %(board_id)s;",
         {"board_id": board_id},
     )
 
-    return matching_cards
-
 
 def get_statuses():
-    statuses = data_manager.execute_select(
-        """
-        select * from statuses
-        ;
-        """
-    )
-    return statuses
+    return data_manager.execute_select("SELECT * FROM statuses;")
 
 
 def insert_board(title):
     return data_manager.execute_select(
-        """
-        insert  into boards(title)
-        values(%(title)s)
-        returning id, title
-        ;
-        """,
+        "INSERT INTO boards (title) VALUES (%(title)s) RETURNING id, title;",
         {"title": title},
         fetchall=False,
     )
@@ -79,12 +42,15 @@ def insert_board(title):
 def update_board_title(id, title):
     return data_manager.execute_select(
         """
-        update boards
-        set title = %(title)s
-        where id = %(id)s
-        returning id, title
+            UPDATE boards
+            SET title = %(title)s
+            WHERE id = %(id)s
+            RETURNING id, title;
         """,
-        {"id": id, "title": title},
+        {
+            "id": id,
+            "title": title,
+        },
         fetchall=False,
     )
 
@@ -92,10 +58,9 @@ def update_board_title(id, title):
 def insert_card(board_id, status_id, title, card_order):
     return data_manager.execute_select(
         """
-        insert  into cards(board_id, status_id, title, card_order)
-        values(%(board_id)s, %(status_id)s, %(title)s, %(card_order)s)
-        returning id, board_id, status_id, title, card_order
-        ;
+            INSERT INTO cards(board_id, status_id, title, card_order)
+            VALUES (%(board_id)s, %(status_id)s, %(title)s, %(card_order)s)
+            RETURNING id, board_id, status_id, title, card_order;
         """,
         {
             "board_id": board_id,
@@ -110,12 +75,15 @@ def insert_card(board_id, status_id, title, card_order):
 def update_card_title(id, title):
     return data_manager.execute_select(
         """
-        update cards
-        set title = %(title)s
-        where id = %(id)s
-        returning id, board_id, status_id, card_order, title
+            UPDATE cards
+            SET title = %(title)s
+            WHERE id = %(id)s
+            RETURNING id, board_id, status_id, card_order, title;
         """,
-        {"id": id, "title": title},
+        {
+            "id": id,
+            "title": title,
+        },
         fetchall=False,
     )
 
@@ -123,11 +91,14 @@ def update_card_title(id, title):
 def update_card_status(id, status_id):
     return data_manager.execute_select(
         """
-        update cards
-        set status_id = %(status_id)s
-        where id = %(id)s
-        returning status_id, id
+            UPDATE cards
+            SET status_id = %(status_id)s
+            WHERE id = %(id)s
+            RETURNING status_id, id;
         """,
-        {"status_id": status_id, "id": id},
+        {
+            "status_id": status_id,
+            "id": id,
+        },
         fetchall=False,
     )
